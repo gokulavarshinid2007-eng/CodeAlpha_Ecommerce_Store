2e58cc8a145d5a1d3c74b1ac1b739521308b4867
gokulavarshinid2007-eng <gokulavarshini.d2007@gmail.com>
2026-06-19 12:22:04 +0530

    CodeAlpha Internship Ecommerce Project

---
+++ README.md
@@ -0,0 +1,18 @@
+# CodeAlpha Ecommerce Store
+
+A Full Stack Ecommerce Website built using React.js and Node.js.
+
+## Features
+
+- User Authentication
+- Product Listing
+- Shopping Cart
+- Checkout System
+
+## Modified By
+
+Varshini
+
+## Internship Project
+
+Submitted for CodeAlpha Internship. 
---
+++ frontend/src/components/footer/footer.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./footer.styles.scss";
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Copyrights &copy; 2020 . design by{" "}
+        <a href="https://www.linkedin.com/in/varshini-d-685847380">CodeAph_Varashini</a>
+      </p>
+    </footer>
+  );
